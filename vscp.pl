c468561378dae4d96c82fe675e98ba41c8c36023
Andrei D <lagged@gmail.com>
2016-11-13 15:53:55 +0100

    cleanup

--- vscp.pl
+++ vscp.pl
@@ -4,7 +4,7 @@
 # binary for domlog and bytelog writeouts. This helps cPanel track
 # bandwidth usage, and include previously missed cache hits in
 # statistical log processing. Such as Logaholic, AWStats, Webalizer
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -20,18 +20,20 @@
 
 use strict;
 use warnings;
+
 # Interval in seconds for /etc/userdatadomains checks
 my $userdata_poll_frequency = "60";
 
 # Custom Port header set in Varnish for the request target (usually 80/443).
-#  It's is mainly used for diferentiating SSL downgraded requests.
-my $varnish_port_header     = 'X-Port';
+# It's is mainly used for differentiating SSL downgraded requests.
+my $varnish_port_header = 'X-Port';
 
 # Client IP header set in Varnish. Usually X-Forwarded-For (beware of multiple IP entries if using the default xforwardedfor)
-my $varnish_ip_header       = 'X-Real-IP';
+my $varnish_ip_header = 'X-Real-IP';
 
-chomp(my $piped_logging = `grep ^enable_piped_logs\=1 /var/cpanel/cpanel.config`);
-if (!$piped_logging) {
+chomp( my $piped_logging =
+      `grep ^enable_piped_logs\=1 /var/cpanel/cpanel.config` );
+if ( !$piped_logging ) {
     print qq{
     Piped logging is currently disabled. To enable follow the steps below to enable.
     
@@ -50,13 +52,20 @@ if (!$piped_logging) {
 }
 
 use Sys::Hostname;
-my $host        = hostname;
-my $vncsa_pid   = open( VNCSA,'varnishncsa -F "%{Varnish:hitmiss}x:::%b:::%{HOST}i:::%{X-Port}i %{X-Real-IP}i %l %u %t \"%m %U%q %H\" %s %b \"%{Referer}i\" \"%{User-agent}i\"" |') or die "Couldn't fork: $!\n";
-my $split_logs  = open( SPLITLOGS,"| /usr/local/cpanel/bin/splitlogs --main=${host} --mainout=/usr/local/apache/logs/access_log") or die "Couldn't fork: $!\n";
-my $split_bytes =  open( SPLITBYTES,    "| /usr/local/cpanel/bin/splitlogs --main=${host} --suffix=-bytes_log" )  or die "Couldn't fork: $!\n";
+my $host      = hostname;
+my $vncsa_pid = open( VNCSA,
+'varnishncsa -F "%{Varnish:hitmiss}x:::%b:::%{HOST}i:::%{X-Port}i %{X-Real-IP}i %l %u %t \"%m %U%q %H\" %s %b \"%{Referer}i\" \"%{User-agent}i\"" |'
+) or die "Couldn't fork: $!\n";
+my $split_logs = open( SPLITLOGS,
+"| /usr/local/cpanel/bin/splitlogs --main=${host} --mainout=/usr/local/apache/logs/access_log"
+) or die "Couldn't fork: $!\n";
+my $split_bytes =
+  open( SPLITBYTES,
+    "| /usr/local/cpanel/bin/splitlogs --main=${host} --suffix=-bytes_log" )
+  or die "Couldn't fork: $!\n";
 my %domlogs;
 my $loaded_md5 = "";
-my $debug = '0';
+my $debug      = '0';
 my $check_time = time();
 
 while (<VNCSA>) {
@@ -128,7 +137,6 @@ sub get_log() {
     }
     else {
         $check =~ s/^[\w-]+\./\*\./;    # strip subdomain for wildcard check
-                                        #$check = '*.'.$check;
         if ( $domlogs{$check} ) {
             return $domlogs{$check};
         }
